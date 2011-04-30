3a449607b4c199ea9bc84e6befe5c0d97ee45806
nolka <i@xternalx.com>
2011-05-01 03:09:51 +0800

    small error code fix

--- options.py
+++ options.py
@@ -1,6 +1,6 @@
 
 #user id in api
-USER_ID         = "4fafa70cd67f123101dd6c39ae66fs29def11aef123"
+USER_ID         = "someUserId"
 #api secret
-USER_SECRET     = "i@xternalx.comi@xternalx.com"
+USER_SECRET     = "someSecret"
 
